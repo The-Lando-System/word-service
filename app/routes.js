4817a553a23cc847d8d2b4c31e332825caa4c9fd
Matt <matt.voget@gmail.com>
2015-12-20 13:39:02 -0700

    enabled cors

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,11 @@ var fs = require('fs');
 var words = fs.readFileSync('./resources/dictionary.txt', 'utf-8').split('\n');
 
 module.exports = function(app) {
+	app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
 	app.get('/dictionary', function(req,res){
 		res.send({ 'words': words });
 	});
