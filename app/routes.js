dc22fd563881ac003a5bb0e11ace96b62fe5dca9
Matt <matt.voget@gmail.com>
2015-12-25 17:01:28 -0700

    support for different length dictionaries

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var words = fs.readFileSync('./resources/dictionary.txt', 'utf-8').split('\n');
+words = words.filter(function(n){ return n != "" });
 
 module.exports = function(app) {
 	app.use(function(req, res, next) {
@@ -11,6 +12,19 @@ module.exports = function(app) {
 	});
 	app.get('/dictionary', function(req,res){
 		res.send({ 'words': words });
+	});
+	app.get('/dictionary/:size', function(req,res){
+		if (req.params.size >= 1 && req.params.size < 28){
+			var newWords = [];
+			for (var i=0; i<words.length; i++){
+				if (words[i].length <= req.params.size){
+					newWords.push(words[i]);
+				}
+			}
+			res.send({ 'words': newWords });
+		} else {
+			res.send({ 'words': words });
+		}
 	});
 	app.get('/random-word', function(req,res){
 		var index = Math.floor(Math.random() * words.length);
